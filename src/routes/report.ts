7ff30cbd766312eb8a80538303c7080c84cada1b
LukeHalasy <lhalasy@gmail.com>
2018-10-11 23:32:16 -0400

    Added more robust aggregation functions, promises, and grouped promises in Promise.all

--- src/routes/report.ts
+++ src/routes/report.ts
@@ -5,242 +5,289 @@ import { ObjectId, Db } from 'mongodb';
 import { Event } from '../models/event';
 import { Member, IMemberModel } from '../models/member';
 import { auth, hasPermissions } from '../middleware/passport';
-import {
-    successRes,
-    errorRes,
-    sendAccountCreatedEmail,
-    hasPermission
-} from '../utils';
+import { successRes, errorRes, sendAccountCreatedEmail, hasPermission } from '../utils';
 export const router = express.Router();
 
 // TODO: Add auth to routes
 // TODO: Add permissions to routes
 
-function formatDates(aggr: any[]): Object {
-    var res = {};
+function formatDates(aggr: any[]): object {
+	const res = {};
 
-    for (var i = 0; i < aggr.length; i++) {
-        if (aggr[i]._id.month < 10) {
-            res[`0${aggr[i]._id.month}/${aggr[i]._id.year}`] = aggr[i].count;
-        } else {
-            res[`${aggr[i]._id.month}/${aggr[i]._id.year}`] = aggr[i].count;
-        }
-    }
-    return res;
-}
-
-function monthDiff(d1, d2): number {
-    var months;
-    months = (d2.getFullYear() - d1.getFullYear()) * 12;
-    months -= d1.getMonth() + 1;
-    months += d2.getMonth();
-    return months <= 0 ? 0 : months;
+	for (const date of aggr) {
+		if (date._id.month < 10) {
+			res[`0${date._id.month}/${date._id.year}`] = date.count;
+		} else {
+			res[`${date._id.month}/${date._id.year}`] = date.count;
+		}
+	}
+	return res;
 }
 
 router.get('/members', async (req, res, next) => {
-    try {
-        let majorsAggregationResult = await Member.aggregate([{ $match: { major: { $ne: null } } }, { $sortByCount: "$major" }], function (err, members) {
-            if (err) {
-                throw err;
-            }
-        });
+	try {
+		const result = await Promise.all([
+			Member.aggregate([
+				{
+					$facet: {
+						major: [{ $match: { major: { $ne: null } } }, { $sortByCount: '$major' }],
+						class: [
+							{ $match: { graduationYear: { $in: [2019, 2020, 2021, 2022] } } },
+							{ $group: { _id: '$graduationYear', count: { $sum: 1 } } },
+							{ $sort: { _id: -1 } }
+						],
+						numPeoplePerDateJoined: [
+							{
+								$addFields: {
+									month: { $month: '$createdAt' },
+									year: { $year: '$createdAt' }
+								}
+							},
+							{
+								$group: { _id: { month: '$month', year: '$year' }, count: { $sum: 1 } }
+							},
+							{ $sort: { '_id.year': 1, '_id.month': 1 } }
+						],
+						membersEventAttendance: [
+							{ $match: { events: { $ne: null } } },
+							{
+								$group: { _id: { numEvents: { $size: '$events' } }, count: { $sum: 1 } }
+							},
+							{ $sort: { _id: 1 } }
+						]
+					}
+				}
+			]).exec(),
+			// Event attendance per month
+			Event.aggregate([
+				{ $match: { members: { $ne: null } } },
+				{
+					$addFields: {
+						month: { $month: '$eventTime' },
+						year: { $year: '$eventTime' },
+						numAtten: { $size: '$members' }
+					}
+				},
+				{
+					$group: {
+						_id: { month: '$month', year: '$year' },
+						count: { $sum: '$numAtten' }
+					}
+				},
+				{ $sort: { '_id.year': 1, '_id.month': 1 } }
+			]).exec()
+		]);
 
-        //format the response
-        const majorData = {};
+		console.log('res', result[1]);
+		// format the response
+		const majorData = {};
+		const majorsAggregationResult = result[0][0].major;
+		for (const major of majorsAggregationResult) {
+			// Format the major name to be the first letters of every word
+			// if the major is more than two words
+			// ex: Computer Science -> CS
+			const individualWords = major._id.split(' ');
+			if (individualWords.length >= 2) {
+				let shortenedMajorName = '';
+				for (const word of individualWords) {
+					shortenedMajorName += word[0];
+				}
 
-        for (let i = 0; i < majorsAggregationResult.length; i++) {
-            // Format the major name to be the first letters of every word
-            // if the major is more than two words
-            // ex: Computer Science -> CS
-            var individualWords = majorsAggregationResult[i]._id.split(" ");
-            if (individualWords.length >= 2) {
-                var shortenedMajorName = "";
-                for (var j = 0; j < individualWords.length; j++) {
-                    shortenedMajorName += individualWords[j][0];
-                }
+				majorData[shortenedMajorName] = major.count;
+			} else {
+				majorData[major._id] = major.count;
+			}
+		}
 
-                majorData[shortenedMajorName] = majorsAggregationResult[i].count;
-            } else {
-                majorData[majorsAggregationResult[i]._id] = majorsAggregationResult[i].count;
-            }
-        }
+		// format the response
+		const classData = {
+			2022: 0,
+			2021: 0,
+			2020: 0,
+			2019: 0
+		};
 
-        let classAggregationResult = await Member.aggregate([{ $match: { graduationYear: { $in: [2019, 2020, 2021, 2022] } } }, { $group: { "_id": "$graduationYear", "count": { "$sum": 1 } } }, { $sort: { _id: -1 } }], function (err, members) {
-            if (err) {
-                throw err;
-            }
-        });
+		const classAggregationResult = result[0][0].class;
+		for (const classElement of classAggregationResult) {
+			classData[classElement._id] = classElement.count;
+		}
 
-        //format the response
-        const classData = {
-            2022: 0,
-            2021: 0,
-            2020: 0,
-            2019: 0
-        };
+		const numPeoplePerDateJoinedAggregationResult = result[0][0].numPeoplePerDateJoined;
+		const numPeoplePerDateJoinedData = formatDates(numPeoplePerDateJoinedAggregationResult);
 
-        for (let i = 0; i < classAggregationResult.length; i++) {
-            classData[classAggregationResult[i]._id] = classAggregationResult[i].count;
-        }
+		// Cumulative Date Joined Data
+		for (let i = 1; i < numPeoplePerDateJoinedAggregationResult.length; i++) {
+			numPeoplePerDateJoinedAggregationResult[i].count +=
+				numPeoplePerDateJoinedAggregationResult[i - 1].count;
+		}
 
-        let numPeoplePerDateJoinedAggregationResult = await Member.aggregate([{ $addFields: { month: { $month: "$createdAt" }, year: { $year: "$createdAt" } } }, { $group: { _id: { month: "$month", year: "$year" }, count: { $sum: 1 } } }, { $sort: { "_id.year": 1, "_id.month": 1 } }], function (err, members) {
-            if (err) {
-                throw err;
-            }
-        });
+		const cumulativeDateJoinedData = {
+			'07/16': 0,
+			...formatDates(numPeoplePerDateJoinedAggregationResult)
+		};
 
-        const numPeoplePerDateJoinedData = formatDates(numPeoplePerDateJoinedAggregationResult);
+		const membersEventAttendanceData = [];
+		const membersEventAttendanceAggregationResult = result[0][0].membersEventAttendance;
 
-        // Cumulative Date Joined Data
-        for (var i = 1; i < numPeoplePerDateJoinedAggregationResult.length; i++) {
-            numPeoplePerDateJoinedAggregationResult[i].count += numPeoplePerDateJoinedAggregationResult[i - 1].count;
-        }
+		for (const eventAttendance of membersEventAttendanceAggregationResult) {
+			if (eventAttendance._id.numEvents >= 10) {
+				if (!membersEventAttendanceData['>10']) {
+					membersEventAttendanceData['>10'] = eventAttendance.count;
+				} else {
+					membersEventAttendanceData['>10'] += eventAttendance.count;
+				}
+			} else {
+				membersEventAttendanceData[eventAttendance._id.numEvents] = eventAttendance.count;
+			}
+		}
 
-        const cumulativeDateJoinedData = { "07/16": 0, ...formatDates(numPeoplePerDateJoinedAggregationResult) };
+		const eventAttendancePerMonthAggregationResult = result[1];
+		const eventAttendancePerMonthData = formatDates(eventAttendancePerMonthAggregationResult);
 
-        let membersEventAttendanceAggregationResult = await Member.aggregate([{ $match: { events: { $ne: null } } }, { $group: { "_id": { numEvents: { $size: "$events" } }, "count": { "$sum": 1 } } }, { $sort: { _id: 1 } }], function (err, members) {
-            if (err) {
-                throw err;
-            }
-        });
-
-        const membersEventAttendanceData = {};
-
-        for (let i = 0; i < membersEventAttendanceAggregationResult.length; i++) {
-            if (membersEventAttendanceAggregationResult[i]._id.numEvents >= 10) {
-                if (!membersEventAttendanceData[">10"]) {
-                    membersEventAttendanceData[">10"] = membersEventAttendanceAggregationResult[i].count;
-                } else {
-                    membersEventAttendanceData[">10"] += membersEventAttendanceAggregationResult[i].count;
-                }
-            } else {
-                membersEventAttendanceData[membersEventAttendanceAggregationResult[i]._id.numEvents] = membersEventAttendanceAggregationResult[i].count;
-            }
-        }
-
-        let eventAttendancePerMonthAggregationResult = await Event.aggregate([{ $match: { members: { $ne: null } } }, { $addFields: { month: { $month: "$eventTime" }, year: { $year: "$eventTime" }, numAtten: { $size: "$members" } } }, { $group: { _id: { month: "$month", year: "$year" }, count: { $sum: "$numAtten" } } }, { $sort: { "_id.year": 1, "_id.month": 1 } }], function (err, members) {
-            if (err) {
-                throw err;
-            }
-        });
-
-        const eventAttendancePerMonthData = formatDates(eventAttendancePerMonthAggregationResult);
-
-        return successRes(res, {
-            classData,
-            majorData,
-            numPeoplePerDateJoinedData,
-            cumulativeDateJoinedData,
-            membersEventAttendanceData,
-            eventAttendancePerMonthData
-        });
-    } catch (error) {
-        return errorRes(res, 500, error);
-    }
+		return successRes(res, {
+			classData,
+			majorData,
+			numPeoplePerDateJoinedData,
+			cumulativeDateJoinedData,
+			membersEventAttendanceData,
+			eventAttendancePerMonthData
+		});
+	} catch (error) {
+		return errorRes(res, 500, error);
+	}
 });
 
 router.get('/event/:id', async (req, res, next) => {
-    try {
-        //Get individual event
-        if (!ObjectId.isValid(req.params.id))
-            return errorRes(res, 400, 'Invalid event ID');
-        const event = await Event.findById(req.params.id)
-            .populate({
-                path: 'members',
-                model: Member
-            })
+	try {
+		// Get individual event
+		if (!ObjectId.isValid(req.params.id)) return errorRes(res, 400, 'Invalid event ID');
 
-        const eventName = event.name;
+		const eventAndEventBeforeIds = await Promise.all([
+			Event.findById(req.params.id)
+				.populate({
+					path: 'members',
+					model: Member
+				})
+				.exec(),
+			Event.find({}, '_id eventTime').exec()
+		]);
 
-        const majorsAggregationResult = await Member.aggregate([{ $match: { _id: { $in: event.members }, major: { $ne: null } } }, { $sortByCount: "$major" }], function (err, members) {
-            if (err) {
-                throw err;
-            }
-        })
+		const event = eventAndEventBeforeIds[0];
+		const eventName = event.name;
 
-        //format the response
-        const majorData = {
-            "CS": 0,
-            "CIT": 0,
-            "FYE": 0,
-            "ECE": 0,
-            "EE": 0,
-            "Math": 0,
-            "CGT": 0,
-            "ME": 0,
-            "Other": 0
-        };
+		const eventIdsAndTimes = eventAndEventBeforeIds[1];
+		const eventsBeforeIds: any[] = new Array();
+		for (const eventIdAndTime of eventIdsAndTimes) {
+			if (eventIdAndTime.eventTime < event.eventTime) {
+				eventsBeforeIds.push(new ObjectId(eventIdAndTime._id));
+			}
+		}
 
-        for (let i = 0; i < majorsAggregationResult.length; i++) {
-            // Format the major name to be the first letters of every word
-            // if the major is more than two words
-            // ex: Computer Science -> CS
-            var individualWords = majorsAggregationResult[i]._id.split(" ");
-            if (individualWords.length >= 2) {
-                var shortenedMajorName = "";
-                for (var j = 0; j < individualWords.length; j++) {
-                    shortenedMajorName += individualWords[j][0];
-                }
+		const result = await Member.aggregate([
+			{
+				$facet: {
+					major: [
+						{ $match: { _id: { $in: event.members }, major: { $ne: null } } },
+						{ $sortByCount: '$major' }
+					],
+					class: [
+						{
+							$match: {
+								_id: { $in: event.members },
+								graduationYear: { $in: [2019, 2020, 2021, 2022] }
+							}
+						},
+						{ $group: { _id: '$graduationYear', count: { $sum: 1 } } },
+						{ $sort: { _id: 1 } }
+					],
+					membersEventAttendance: [
+						{ $match: { _id: { $in: event.members } } },
+						{
+							$addFields: {
+								numEvents: {
+									$size: {
+										$filter: {
+											input: '$events',
+											as: 'id',
+											cond: { $in: ['$$id', eventsBeforeIds] }
+										}
+									}
+								}
+							}
+						},
+						{ $group: { _id: { numEvents: '$numEvents' }, count: { $sum: 1 } } },
+						{ $sort: { _id: 1 } }
+					]
+				}
+			}
+		]).exec();
 
-                majorData[shortenedMajorName] = majorsAggregationResult[i].count;
-            } else {
-                majorData[majorsAggregationResult[i]._id] = majorsAggregationResult[i].count;
-            }
-        }
+		// format the response
+		const majorData = {
+			CS: 0,
+			CIT: 0,
+			FYE: 0,
+			ECE: 0,
+			EE: 0,
+			Math: 0,
+			CGT: 0,
+			ME: 0,
+			Other: 0
+		};
+		const majorsAggregationResult = result[0].major;
 
-        let classAggregationResult = await Member.aggregate([{ $match: { _id: { $in: event.members }, graduationYear: { $in: [2019, 2020, 2021, 2022] } } }, { $group: { "_id": "$graduationYear", "count": { "$sum": 1 } } }, { $sort: { _id: 1 } }], function (err, members) {
-            if (err) {
-                throw err;
-            }
-        });
+		for (const major of majorsAggregationResult) {
+			// Format the major name to be the first letters of every word
+			// if the major is more than two words
+			// ex: Computer Science -> CS
+			const individualWords = major._id.split(' ');
+			if (individualWords.length >= 2) {
+				let shortenedMajorName = '';
+				for (const word of individualWords) {
+					shortenedMajorName += word[0];
+				}
 
-        //format the response
-        const classData = {
-            2022: 0,
-            2021: 0,
-            2020: 0,
-            2019: 0
-        };
+				majorData[shortenedMajorName] = major.count;
+			} else {
+				majorData[major._id] = major.count;
+			}
+		}
 
-        for (let i = 0; i < classAggregationResult.length; i++) {
-            classData[classAggregationResult[i]._id] = classAggregationResult[i].count;
-        }
+		// format the response
+		const classData = {
+			2022: 0,
+			2021: 0,
+			2020: 0,
+			2019: 0
+		};
+		const classAggregationResult = result[0].class;
 
-        const results = await Event.find({}, "_id eventTime").exec();
-        var eventsBeforeIds: any[] = new Array();
-        for (var i = 0; i < results.length; i++) {
-            if (results[i].eventTime < event.eventTime) {
-                eventsBeforeIds.push(new ObjectId(results[i]._id));
-            }
-        }
+		for (const classElement of classAggregationResult) {
+			classData[classElement._id] = classElement.count;
+		}
 
-        let membersEventAttendanceAggregationResult = await Member.aggregate([{ $match: { _id: { $in: event.members } } }, { $addFields: { numEvents: { $size: { $filter: { input: "$events", as: "id", cond: { $in: ["$$id", eventsBeforeIds] } } } } } }, { $group: { "_id": { numEvents: "$numEvents" }, "count": { "$sum": 1 } } }, { $sort: { _id: 1 } }], function (err, members) {
-            if (err) {
-                throw err;
-            }
-        })
+		const membersEventAttendanceData = {};
+		const membersEventAttendanceAggregationResult = result[0].membersEventAttendance;
 
-        const membersEventAttendanceData = {};
+		for (const eventAttendance of membersEventAttendanceAggregationResult) {
+			if (eventAttendance._id.numEvents >= 10) {
+				if (!membersEventAttendanceData['>10']) {
+					membersEventAttendanceData['>10'] = eventAttendance.count;
+				} else {
+					membersEventAttendanceData['>10'] += eventAttendance.count;
+				}
+			} else {
+				membersEventAttendanceData[eventAttendance._id.numEvents] = eventAttendance.count;
+			}
+		}
 
-        for (let i = 0; i < membersEventAttendanceAggregationResult.length; i++) {
-            if (membersEventAttendanceAggregationResult[i]._id.numEvents >= 10) {
-                if (!membersEventAttendanceData[">10"]) {
-                    membersEventAttendanceData[">10"] = membersEventAttendanceAggregationResult[i].count;
-                } else {
-                    membersEventAttendanceData[">10"] += membersEventAttendanceAggregationResult[i].count;
-                }
-            } else {
-                membersEventAttendanceData[membersEventAttendanceAggregationResult[i]._id.numEvents] = membersEventAttendanceAggregationResult[i].count;
-            }
-        }
-
-        return successRes(res, {
-            eventName,
-            majorData,
-            classData,
-            membersEventAttendanceData
-        });
-    } catch (error) {
-        return errorRes(res, 500, error);
-    }
+		return successRes(res, {
+			eventName,
+			majorData,
+			classData,
+			membersEventAttendanceData
+		});
+	} catch (error) {
+		return errorRes(res, 500, error);
+	}
 });

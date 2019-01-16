6778ac763fb31522721a01e4a8e3ecd6493f469a
Ashwin Gokhale <ashwin.gokhale98@gmail.com>
2019-01-16 15:44:09 -0500

    Fixed mongo download error for testing

--- test/globals/mongo.ts
+++ test/globals/mongo.ts
@@ -3,11 +3,7 @@ import MongodbMemoryServer from 'mongodb-memory-server';
 let mongod: MongodbMemoryServer;
 
 export const setup = async () => {
-	mongod = new MongodbMemoryServer({
-		binary: {
-			version: '4.0.2'
-		}
-	});
+	mongod = new MongodbMemoryServer();
 	const DB = await mongod.getConnectionString();
 	process.env.DB = DB;
 };
